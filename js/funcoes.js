4638dce6ac1a93cbbebcd55cf2c6bf2aad509056
wesleygdsc <wesleygdsc@gmail.com>
2019-04-01 22:39:38 -0300

    auladeHTML

---
+++ js/funcoes.js
@@ -0,0 +1,15 @@
+J
+function trocaFonteMaior() {
+	document.getElementById("demo").style.fontSize="35px";
+}
+
+function trocaFonteMenor() {
+	document.getElementById("demo").style.fontSize="16px";
+}
+
+function apagarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function naoApagarElemento(){
+	document.getElementById("demo").style.display="block";
+}
